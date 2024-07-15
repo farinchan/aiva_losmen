4591425d6ed16e628634659164088dbfab0b81f0
fajri <fajri@gariskode.com>
2024-07-16 00:29:20 +0700

    update Menu back

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Back\DashboardController;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->name('home');
+
+Route::prefix('back')->name('back.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+
