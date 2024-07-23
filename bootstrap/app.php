72afd070b2c48cf24d313aeebd5062289f4dafcd
fajri <fajri@gariskode.com>
2024-07-23 23:55:49 +0700

    fix route issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn (Request $request) => route('auth.login'));
+        $middleware->alias([
+            'role' => \App\Http\Middleware\Role::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -21,42 +21,42 @@ Route::put('/my-profile/update', [ProfileController::class, 'update'])->name('pr
 
 
 Route::prefix('auth')->name('auth.')->group(function () {
-    Route::get('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/login', [AuthController::class, 'loginProcess'])->name('login.process');
-    Route::get('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/register', [AuthController::class, 'registerProcess'])->name('register.process');
-    Route::get('/forget-password', [AuthController::class, 'forgetPassword'])->name('forget-password');
-    Route::post('/forget-password', [AuthController::class, 'forgetPasswordProcess'])->name('forget-password.process');
-    Route::get('/reset-password/{token}', [AuthController::class, 'resetPassword'])->name('reset-password');
-    Route::post('/reset-password/{token}', [AuthController::class, 'resetPasswordProcess'])->name('reset-password.process');
-    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/login', [AuthController::class, 'login'])->middleware('guest')->name('login');
+    Route::post('/login', [AuthController::class, 'loginProcess'])->middleware('guest')->name('login.process');
+    Route::get('/register', [AuthController::class, 'register'])->middleware('guest')->name('register');
+    Route::post('/register', [AuthController::class, 'registerProcess'])->middleware('guest')->name('register.process');
+    Route::get('/forget-password', [AuthController::class, 'forgetPassword'])->middleware('guest')->name('forget-password');
+    Route::post('/forget-password', [AuthController::class, 'forgetPasswordProcess'])->middleware('guest')->name('forget-password.process');
+    Route::get('/reset-password/{token}', [AuthController::class, 'resetPassword'])->middleware('guest')->name('reset-password');
+    Route::post('/reset-password/{token}', [AuthController::class, 'resetPasswordProcess'])->middleware('guest')->name('reset-password.process');
+    Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 });
 
-Route::prefix('back')->name('back.')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::prefix('back')->name('back.')->middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('role:admin|owner')->name('dashboard');
 
-    Route::prefix('metode-pembayaran')->name('metode-pembayaran.')->group(function () {
+    Route::prefix('metode-pembayaran')->name('metode-pembayaran.')->middleware('role:admin')->group(function () {
         Route::get('/', [MetodePembayaranController::class, 'index'])->name('index');
         Route::post('/store', [MetodePembayaranController::class, 'store'])->name('store');
         Route::put('/update/{id}', [MetodePembayaranController::class, 'update'])->name('update');
         Route::delete('/destroy/{id}', [MetodePembayaranController::class, 'destroy'])->name('destroy');
     });
 
-    Route::prefix('fasilitas-kamar')->name('fasilitas-kamar.')->group(function () {
+    Route::prefix('fasilitas-kamar')->name('fasilitas-kamar.')->middleware('role:admin')->group(function () {
         Route::get('/', [FasilitasKamarController::class, 'index'])->name('index');
         Route::post('/store', [FasilitasKamarController::class, 'store'])->name('store');
         Route::put('/update/{id}', [FasilitasKamarController::class, 'update'])->name('update');
         Route::delete('/destroy/{id}', [FasilitasKamarController::class, 'destroy'])->name('destroy');
     });
 
-    Route::prefix('tipe-kamar')->name('tipe-kamar.')->group(function () {
+    Route::prefix('tipe-kamar')->name('tipe-kamar.')->middleware('role:admin')->group(function () {
         Route::get('/', [TipeKamarController::class, 'index'])->name('index');
         Route::post('/store', [TipeKamarController::class, 'store'])->name('store');
         Route::put('/update/{id}', [TipeKamarController::class, 'update'])->name('update');
         Route::delete('/destroy/{id}', [TipeKamarController::class, 'destroy'])->name('destroy');
     });
 
-    Route::prefix('kamar')->name('kamar.')->group(function () {
+    Route::prefix('kamar')->name('kamar.')->middleware('role:admin')->group(function () {
         Route::get('/', [KamarController::class, 'index'])->name('index');
         Route::get('/create', [KamarController::class, 'create'])->name('create');
         Route::post('/store', [KamarController::class, 'store'])->name('store');
@@ -68,11 +68,11 @@ Route::prefix('back')->name('back.')->group(function () {
         Route::get('/detail/{id}/ulasan', [KamarController::class, 'ulasan'])->name('ulasan');
     });
 
-    Route::prefix('ulasan')->name('ulasan.')->group(function () {
+    Route::prefix('ulasan')->name('ulasan.')->middleware('role:admin')->group(function () {
         Route::get('/', [UlasanController::class, 'index'])->name('index');
     });
 
-    Route::prefix('pengguna')->name('pengguna.')->group(function () {
+    Route::prefix('pengguna')->name('pengguna.')->middleware('role:admin')->group(function () {
         Route::get('/pelanggan', [UserController::class, 'pelanggan'])->name('pelanggan');
         Route::post('/pelanggan/store', [UserController::class, 'pelangganStore'])->name('pelanggan.store');
         Route::put('/pelanggan/update/{id}', [UserController::class, 'pelangganUpdate'])->name('pelanggan.update');
